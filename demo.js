f64807622b5c56d08e482037ef603b725ede454e
Joseph Gentle <me@josephg.com>
2017-10-04 01:44:04 +1100

    Lots of styling changes. Start of drag drop

--- demo.js
+++ demo.js
@@ -1,7 +1,8 @@
 const choo = require('choo')
 const html = require('choo/html')
-const {editor, middleware} = require('./index')
+const makeEditor = require('./index')
 const css = require('sheetify')
+const json = require('ot-json1').type
 
 css`
 html {
@@ -18,19 +19,56 @@ body, h1, h2, h3, h4, h5, h6, p, ol, ul {
 *, *:before, *:after {
   box-sizing: inherit;
 }
+
+#editor {
+  max-width: 600px;
+  margin: 2em auto 0 auto;
+}
+
+h1, h2 {
+  margin-top: 2em;
+}
 `
 
 const app = choo()
 
-app.use(middleware('expand'))
+// {editor, middleware}
+
+// app.use(middleware)
+
+let editorView
+
+app.use((state, emitter) => {
+  state.data = {x:45, z: null, y:'hi there', bool:true, ff:false, alist: ['a', 'b', {x:10}]}
+  state.ops = null
+
+  const editor = makeEditor((op) => {
+    state.data = json.apply(state.data, op)
+    console.log('applying op', op, '->', JSON.stringify(state.data))
+    state.ops = json.compose(state.ops, op)
+    emitter.emit('render')
+  })
+
+  app.use(editor.middleware)
+  editorView = editor.view
+})
+
 app.route('/', mainView)
+app.route('/jsoneditor', mainView)
 app.mount('body')
 
 function mainView (state, emit) {
   return html`
     <body>
-      <h1>JSON Editor demo!</h1>
-      ${editor(state.expand, {x:45, z: null, y:'hi there', bool:true, ff:false, alist: ['a', 'b', {x:10}]}, emit)}
+      <div id=editor>
+        <h1>JSON Editor demo!</h1>
+        ${editorView(state.data, emit)}
+
+        <h2>Aggregate operation:</h2>
+        <pre style='font-family: monospace;'>${JSON.stringify(state.ops, null, 2)}</pre>
+      </div>
+      <div style=''>
+      </div>
     </body>
   `
 }
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const html = require('choo/html')
-const json = require('ot-json1').type
 const css = require('sheetify')
 
 const container = css`
@@ -14,15 +13,121 @@ const getPath = link => {
   return p
 }
 
+// const getAtPath = (data, path) => {
+//   let container = data
+//   for (let i = 0; i < path.length; i++) {
+//     if (container == null) return undefined
+//     container = container[path[i]]
+//   }
+//   return container
+// }
+
+// const setAtPath = (data, path, val) => {
+//   let container = data
+//   for (let i = 0; i < path.length - 1; i++) {
+//     container = container[k]
+//     // assert(container).
+//   }
+//   container[path[path.length - 1]] = val
+// }
+
+function strDiffToOp(oldval, newval) {
+  if (oldval === newval) return null;
+
+  let commonStart = 0
+  while (oldval.charAt(commonStart) === newval.charAt(commonStart)) commonStart++
+
+  var commonEnd = 0
+  while (oldval.charAt(oldval.length - 1 - commonEnd) === newval.charAt(newval.length - 1 - commonEnd) &&
+      commonEnd + commonStart < oldval.length && commonEnd + commonStart < newval.length) {
+    commonEnd++
+  }
+
+  const op = commonStart > 0 ? [commonStart] : []
+
+  if (oldval.length !== commonStart + commonEnd) {
+    op.push({d: oldval.length - commonStart - commonEnd})
+    // ctx.remove(commonStart, oldval.length - commonStart - commonEnd);
+  }
+  if (newval.length !== commonStart + commonEnd) {
+    op.push(newval.slice(commonStart, newval.length - commonEnd))
+    // ctx.insert(commonStart, newval.slice(commonStart, newval.length - commonEnd));
+  }
+  return op
+}
+
+const textEditClass = css`
+  :host {
+    font-family: inherit;
+    font-size: inherit;
+    border: none;
+  }
+  :host[type=text] {
+    color: green;
+  }
+  :host[type=number] {
+    color: dodgerblue;
+  }
+  :host:not(:focus) {
+    background-color: transparent;
+  }
+`
+
+const boolEditClass = css`
+  :host {
+    color: darkred;
+    user-select: none;
+  }
+`
+
+const keyClass = css`
+  :host {
+    font-size: 0.9em;
+    font-weight: lighter;
+    color: #444;
+  }
+`
+
+const rowClass = css`
+  :host:hover {
+    background-color: rgba(255,255,255,0.5);
+  }
+`
+
 function valueEditor({link, thing}, emit) {
   // console.log('thingue', thing, 'at', getPath(link))
-  return [html`<span>${thing}</span>`, null]
+  let view
+
+  if (typeof thing === 'boolean') {
+    view = html`<span class='${boolEditClass}'><label>
+      <input type='checkbox' checked=${thing} onchange=${(e) => {
+        const newval = e.target.checked
+        emit('editor op', {path: getPath(link), component: {r:true, i:newval}})
+      }} >${thing}
+    </label></span>`
+  } else if (typeof thing === 'string') {
+    //size='${thing.length}'
+    view = html`<span><input class='${textEditClass}' type=text value="${thing}" oninput=${(e) => {
+      const newval = e.target.value
+      emit('editor op', {path: getPath(link), component: {es: strDiffToOp(thing, newval)}})
+    }} ></span>`
+  } else if (typeof thing === 'number') {
+    view = html`<span><input class='${textEditClass}' type=number value="${thing}" oninput=${(e) => {
+      const newval = e.target.value
+      emit('editor op', {path: getPath(link), component: {r: true, i:+newval}})
+    }} ></span>`
+  } else view = html`<span>${''+thing}</span>`
+
+  return [
+    view,
+    null
+  ]
 }
 
 const disclosureBtn = (state, path, emit) => {
   const toggle = () => emit('editor toggle expand', path)
   // TODO: replace this with a checkbox.
-  const button = html`<button type=button onclick=${toggle}>${state ? 'V' : '>'}</button>`
+  const button = html`<button type='button' onclick=${toggle}>${state ? 'V' : '>'}</button>`
   return button
 }
 
@@ -32,9 +137,9 @@ function objEditor({expand, link, thing}, emit) {
 
   // console.log('obj')
   const toggleBtn = disclosureBtn(expandSelf, getPath(link), emit)
-  const keys = Object.keys(thing)
+  const keys = Object.keys(thing).sort()
   return [
-    html`<span>{ (Object (${keys.length}) ${toggleBtn} ${expandSelf ? '' : '}'}</span>`,
+    html`<span>{ Object (${keys.length}) ${toggleBtn} ${expandSelf ? '' : '}'}</span>`,
     expandSelf ? html`<div>
       <div class='${container}'>
         ${keys.map(k => {
@@ -44,8 +149,8 @@ function objEditor({expand, link, thing}, emit) {
             expand: expandChildren[k]
           }, emit)
 
-          return html`<div>
-            <span>${k}: ${inline}</span>
+          return html`<div class='${rowClass}'>
+            <span><span class='${keyClass}'>${k}:</span> ${inline}</span>
             ${child}
           </div>`
         })}
@@ -53,6 +158,16 @@ function objEditor({expand, link, thing}, emit) {
     </div>` : null
   ]
 }
+
+const listItemClass = css`
+  :host {
+    cursor: pointer;
+    cursor: -webkit-grab;
+  }
+  :host:hover {
+    background-color: rgba(255,255,255,0.5);
+  }
+`
 
 function listEditor({expand, link, thing}, emit) {
   const expandSelf = expand ? expand[0] : true
@@ -69,8 +184,8 @@ function listEditor({expand, link, thing}, emit) {
             link:{parent: link, k:i},
             expand: expandChildren[i],
           }, emit)
-          return html`<div>
-            <span>${i}: ${inline}</span>
+          return html`<div draggable=true class="${listItemClass}" ondragstart=${(e => {console.log(e)})}>
+            <span><span class='${keyClass}'>${i}:</span> ${inline}</span>
             ${child}
           </div>`
         })}
@@ -98,34 +213,57 @@ const mainClass = css`
   }
 `
 
-const editor = exports.editor = (expand, thing, emit) => {
-  const [inline, child] = anyEditor({expand, thing}, emit)
-  return html`<div class='${mainClass}'>
-    <div>${inline}</div>
-    ${child}
-  </div>`
+
+module.exports = (onEdit) => {
+  const expand = [true, null]
+
+  return {
+    middleware(state, emitter) {
+
+      emitter.on('editor toggle expand', (path) => {
+        let e = expand
+        // debugger
+        path.forEach(p => {
+          // The path item is a string for object keys and a number for lists.
+          let children = e[1]
+          if (typeof p === 'string') {
+            if (children == null || Array.isArray(children)) e[1] = children = {}
+          } else {
+            if (children == null || !Array.isArray(children)) e[1] = children = []
+          }
+          if (children[p] == null) children[p] = [true, null]
+          e = children[p]
+        })
+
+        e[0] = !e[0]
+        // console.log('expand', JSON.stringify(expand))
+        emitter.emit('render')
+      })
+
+      emitter.on('editor op', ({path, component}) => {
+        const op = path
+        op.push(component)
+        onEdit(op)
+        // console.log('op', op)
+        // const oldval = getAtPath(state, dataPath)
+        // const newval = json.apply(oldval, op)
+        // setAtPath(state, dataPath, newval)
+        // emitter.emit('render')
+      })
+    },
+
+    view(thing, emit) {
+      // const thing = getAtPath(state, dataPath)
+      const [inline, child] = anyEditor({expand, thing}, emit)
+
+      return html`<div class='${mainClass}'>
+        <div>${inline}</div>
+        ${child}
+      </div>`
+    }
+  }
+
 }
 
-exports.middleware = (key) => (state, emitter) => {
-  if (state[key] == null) state[key] = [true, null]
-  const expand = state[key]
 
-  emitter.on('editor toggle expand', (path) => {
-    let e = expand
-    path.forEach(p => {
-      // The path item is a string for object keys and a number for lists.
-      let children = expand[1]
-      if (typeof p === 'string') {
-        if (children == null || Array.isArray(children)) expand[1] = children = {}
-      } else {
-        if (children == null || !Array.isArray(children)) expand[1] = children = []
-      }
-      if (children[p] == null) children[p] = [true, null]
-      e = children[p]
-    })
-
-    e[0] = !e[0]
-    console.log('expand', JSON.stringify(expand))
-    emitter.emit('render')
-  })
-}
+console.log('hi')
